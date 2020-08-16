8ee0197eb369ca4d9f323f86308d94e0f6ccc74d
Amirhossein Azizian <AMIAZ@orsted.dk>
2020-08-16 22:03:16 +0800

    Setup and tear down added to rectangle test

--- tests/RectangleTests.cpp
+++ tests/RectangleTests.cpp
@@ -2,24 +2,33 @@
 #include "../source/Rectangle.hpp"
 using namespace std;
 
-TEST(RectangleTests, ConstructorShouldSetBottomLeftCorner)
+struct RectangleTests : public ::testing::Test
 {
-    Point2D startingPoint = Point2D(2.6, 3.7);
+    Rectangle* rectangle;
+
+    virtual void SetUp() override
+    {
+        Point2D startingPoint = Point2D(2.6, 3.7);
     
-    Rectangle rectangle = Rectangle(startingPoint, 5.7, 6.8);
-    
-    Point2D bottomLeftCorner = rectangle.GetBottomLeftCorner();
+        rectangle = new Rectangle(startingPoint, 5.7, 6.8);
+    }
+
+    virtual void TearDown() override 
+    {
+        delete rectangle;
+    }
+};
+
+TEST_F(RectangleTests, ConstructorShouldSetBottomLeftCorner)
+{
+    Point2D bottomLeftCorner = rectangle -> GetBottomLeftCorner();
     ASSERT_FLOAT_EQ(2.6, bottomLeftCorner.getX());
     ASSERT_FLOAT_EQ(3.7, bottomLeftCorner.getY());
 }
 
-TEST(RectangleTests, ConstructorShouldSetBottomRightCorner)
+TEST_F(RectangleTests, ConstructorShouldSetBottomRightCorner)
 {
-    Point2D startingPoint = Point2D(2.6, 3.7);
-    
-    Rectangle rectangle = Rectangle(startingPoint, 5.7, 6.8);
-    
-    Point2D bottomRightCorner = rectangle.GetBottomRightCorner();
+    Point2D bottomRightCorner = rectangle -> GetBottomRightCorner();
     ASSERT_FLOAT_EQ(8.3, bottomRightCorner.getX());
     ASSERT_FLOAT_EQ(3.7, bottomRightCorner.getY());
 }
